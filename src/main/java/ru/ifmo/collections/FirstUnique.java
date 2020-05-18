2070fa0152683de280dcad3b64c3034d7cb5da8c
volyomaS <rastoden@gmail.com>
2020-05-18 15:16:14 +0300

    lab7 done

--- src/main/java/ru/ifmo/collections/FirstUnique.java
+++ src/main/java/ru/ifmo/collections/FirstUnique.java
@@ -1,19 +1,46 @@
 package ru.ifmo.collections;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 /**
  * Design a class which contains integers and returns first unique integer (in order of addition).
  * FirstUniqueTest can be used as an example.
  */
 public class FirstUnique {
+
+    private final LinkedList<Integer> keys = new LinkedList<>();
+    private final HashMap<Integer, Integer> keysCount = new HashMap<>();
+
     public FirstUnique(int[] numbers) {
-        // TODO implement
+        if (numbers == null) {
+            throw new IllegalArgumentException("Array of numbers shouldn't be null");
+        }
+        for (int number : numbers) {
+            if (!keysCount.containsKey(number)) {
+                keysCount.put(number, 1);
+                keys.add(number);
+            } else {
+                keysCount.replace(number, keysCount.get(number) + 1);
+            }
+        }
     }
 
     public int showFirstUnique() {
-        throw new UnsupportedOperationException(); // TODO implement
+        for (int key : keys) {
+            if (keysCount.get(key) == 1) {
+                return key;
+            }
+        }
+        return -1;
     }
 
     public void add(int value) {
-        throw new UnsupportedOperationException(); // TODO implement
+        if (!keysCount.containsKey(value)) {
+            keysCount.put(value, 1);
+            keys.add(value);
+        } else {
+            keysCount.replace(value, keysCount.get(value) + 1);
+        }
     }
 }
--- src/main/java/ru/ifmo/collections/LruCache.java
+++ src/main/java/ru/ifmo/collections/LruCache.java
@@ -1,29 +1,44 @@
 package ru.ifmo.collections;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * Represents LRU cache with fixed maximum capacity.
- *
+ * <p>
  * get() should return null if there is no value for a given key.
  * elements() should return number of elements in cache.
- *
+ * <p>
  * This class should not have any other public methods.
- *
+ * <p>
  * Implementing this cache in (almost) the same manner as it was implemented during the lecture will result in extra points.
  */
 public class LruCache<K, V> {
+
+    private final LinkedHashMap<K, V> values;
+    private final int maxCapacity;
+
     public LruCache(int capacity) {
-        // TODO implement
+        if (capacity < 1) {
+            throw new IllegalArgumentException("Capacity should be bigger than 0");
+        }
+        this.values = new LinkedHashMap<>(capacity, 1f, true) {
+            protected boolean removeEldestEntry(Map.Entry eldest) {
+                return size() > maxCapacity;
+            }
+        };
+        this.maxCapacity = capacity;
     }
 
     public V get(K key) {
-        throw new UnsupportedOperationException(); // TODO implement
+        return values.get(key);
     }
 
     public void put(K key, V value) {
-        // TODO implement
+        values.put(key, value);
     }
 
     public int elements() {
-        throw new UnsupportedOperationException(); // TODO implement
+        return values.keySet().size();
     }
 }
